6a70099eb004adc56a65416223b3fb20f7a40ac5
Ruiwen <ruiwen.li@mail.utoronto.ca>
2021-03-03 19:43:07 -0500

    Add cifar10 scripts

---
+++ bash/cifar10/lwf.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ../..
+source online-cl/bin/activate
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/cifar10/cifar10_nc.yml --agent config_CVPR/agent/lwf/lwf.yml
---
+++ bash/cifar10/scr.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ../..
+source online-cl/bin/activate
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/cifar10/cifar10_nc.yml --agent config_CVPR/agent/scr/scr_02k.yml
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/cifar10/cifar10_nc.yml --agent config_CVPR/agent/scr/scr_05k.yml
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/cifar10/cifar10_nc.yml --agent config_CVPR/agent/scr/scr_1k.yml
