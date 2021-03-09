554d90d18e07223787edac3b115cedb12f464eea
Ruiwen <ruiwen.li@mail.utoronto.ca>
2021-03-08 19:32:30 -0500

    Add finetune iid config

---
+++ bash/cifar100/iid_offline.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ../..
+source online-cl/bin/activate
+python -u multiple_run.py --general config_CVPR/general_iid_offline.yml --data config_CVPR/data/cifar100/cifar100_nc.yml --agent config_CVPR/agent/er/finetune.yml
---
+++ bash/mini_imagenet/iid_online.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ../..
+source online-cl/bin/activate
+python -u multiple_run.py --general config_CVPR/general_iid_online.yml --data config_CVPR/data/mini_imagenet/mini_imagenet_nc.yml --agent config_CVPR/agent/er/finetune.yml
