252b70e4a669f17102b4e9fd4b3b4e3545ecf11f
Ruiwen <ruiwen.li@mail.utoronto.ca>
2021-03-10 23:24:30 -0500

    Add bash script

---
+++ bash/mini_imagenet/aser_ncm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ../..
+source online-cl/bin/activate
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/mini_imagenet/mini_imagenet_nc.yml --agent config_CVPR/agent/aser/aser_1k_mini_ncm.yml
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/mini_imagenet/mini_imagenet_nc.yml --agent config_CVPR/agent/aser/aser_2k_mini_ncm.yml
+python -u multiple_run.py --general config_CVPR/general.yml --data config_CVPR/data/mini_imagenet/mini_imagenet_nc.yml --agent config_CVPR/agent/aser/aser_5k_mini_ncm.yml
