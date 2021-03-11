f7de250d8b3186f01ad7062fc0d4a43175345402
Ruiwen <ruiwen.li@mail.utoronto.ca>
2021-03-10 23:28:10 -0500

    Add bash script

--- bash/mini_imagenet/run_gss_ncm.sh
+++ bash/mini_imagenet/run_gss_ncm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbatch --nodes=1 --time=12:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner gss_ncm.sh
+sbatch --nodes=1 --time=36:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner gss_ncm.sh
